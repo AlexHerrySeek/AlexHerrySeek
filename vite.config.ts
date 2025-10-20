9f30e33d4cb33fa15bc517eb31ad2e0076656540
Nguyễn Minh Nhật <nguyentiendino@gmail.com>
2025-10-21 02:16:59 +0700

    fix editor and index

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import viteLegacyPlugin from "@vitejs/plugin-legacy";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import viteLegacyPlugin from '@vitejs/plugin-legacy';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     viteLegacyPlugin({
-      targets: ["defaults", "fully supports es6-module"],
+      targets: ['defaults', 'fully supports es6-module'],
     }),
   ],
   build: {
-    minify: "terser",
+    minify: 'terser',
     terserOptions: {
       parse: {
         html5_comments: false,
       },
       format: {
-        comments: false
-      }
-    }
-  }
-})
+        comments: false,
+      },
+    },
+  },
+  server: {
+    middlewareMode: true,
+    setupMiddlewares(middlewares, server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === '/editor') {
+          res.writeHead(302, { Location: '/editor.html' });
+          res.end();
+        } else {
+          next();
+        }
+      });
+      return middlewares;
+    },
+  },
+});
